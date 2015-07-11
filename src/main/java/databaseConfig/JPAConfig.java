bd3b3eaa4156a90ae5bde7d03fb887d1eac1a54e
unknown <ybrendon@hotmail.com>
2015-07-11 10:53:25 -0700

    majors

---
+++ src/main/java/databaseConfig/JPAConfig.java
@@ -0,0 +1,51 @@
+package databaseConfig;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JPAConfig {
+    
+    private String db;
+
+    public JPAConfig(String db) {
+        this.db = db;
+    }
+
+    public Map Config(){
+        
+        // JDBC driver
+        final String JDBC_DRIVER="com.mysql.jdbc.Driver";
+
+        //local settings (default)
+        String server = "localhost";
+        String port = "3306";
+        String db_user = "root";
+        String db_password = "";
+        
+        //remote settings
+        if(System.getenv("OPENSHIFT_MYSQL_DB_HOST") != null){
+            server = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+            port = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
+            db_user = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME");
+            db_password = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
+        }
+            
+        String db_url="jdbc:mysql://" + server + ":" + port + "/" + this.db;
+
+        Map<String, String> properties = new HashMap<>();
+        properties.put("javax.persistence.jdbc.driver", JDBC_DRIVER);
+        properties.put("javax.persistence.jdbc.url", db_url);
+        properties.put("javax.persistence.jdbc.user", db_user);
+        properties.put("javax.persistence.jdbc.password", db_password);
+        
+        return properties;
+    }
+    
+    public String getDb() {
+        return db;
+    }
+
+    public void setDb(String db) {
+        this.db = db;
+    }
+}
